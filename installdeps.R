e0ad531ce98c7af6f80bae391b8659c3b8528704
Levi Baber <baberlevi@gmail.com>
2019-04-05 14:21:23 -0500

    handle bioconductor packages

--- installdeps.R
+++ installdeps.R
@@ -1,4 +1,3 @@
-
 # set cran mirror
 local({
 r = getOption("repos")
@@ -9,23 +8,23 @@ options(repos = r)
 # function to install r-requirements.txt
 # adapted from https://gist.github.com/cannin/6b8c68e7db19c4902459
 installdeps <- function(file="/opt/app-root/src/r-requirements.txt", lib="/opt/app-root/src/R_libs") {
-  
-  # Install packages 
+
+  # Install packages
   if(!is.null(file)) {
     packages <- utils:::read.table(file, stringsAsFactors = FALSE)
     packages <- packages$V1
   }
-  
+
   if("devtools" %in% rownames(utils:::installed.packages())) {
-    require(devtools)	  
+    require(devtools)
   } else {
-    utils:::install.packages("devtools", lib=lib)  
+    utils:::install.packages("devtools", lib=lib)
   }
-  
+
   if("stringr" %in% rownames(utils:::installed.packages())) {
-    require(stringr)	  
+    require(stringr)
   } else {
-    utils:::install.packages("stringr", lib=lib)  
+    utils:::install.packages("stringr", lib=lib)
   }
 
   for(package in packages) {
@@ -35,7 +34,12 @@ installdeps <- function(file="/opt/app-root/src/r-requirements.txt", lib="/opt/a
     if(!(packageName %in% rownames(utils:::installed.packages()))) {
       if(package %in% rownames(utils:::available.packages())) {
         utils:::install.packages(package, lib=lib)
-      } 
+      } else {
+        require(BiocManager)
+        if(length(BiocManager::available(package))>0) {
+          BiocManager::install("TissueEnrich")
+        }
+      }
     }
   }
 }
