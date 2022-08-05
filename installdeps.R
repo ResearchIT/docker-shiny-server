eb4b25dd7ced158bbc34a8c6294c60ffd9664aef
rassilon <rassilon@automorphism.las.iastate.edu>
2022-08-05 16:30:35 -0500

    Added support for installing packages from github.

--- installdeps.R
+++ installdeps.R
@@ -31,9 +31,15 @@ installdeps <- function(file="/opt/app-root/src/r-requirements.txt", lib="/opt/a
     tmp <- strsplit(package, .Platform$file.sep)[[1]]
     packageName <- tmp[length(tmp)]
 
+    # If it is not already installed...
     if(!(packageName %in% rownames(utils:::installed.packages()))) {
+      # Try to install from known repos (CRAN)...
       if(package %in% rownames(utils:::available.packages())) {
         utils:::install.packages(package, lib=lib)
+      # Or see if we need to install from github...
+      } else if(grepl("^github::", package)) {
+        gitPkg <- strsplit(package, "::")[[1]][2]
+        devtools::install_github(gitPkg)
       } else {
         require(BiocManager)
         if(length(BiocManager::available(package))>0) {
